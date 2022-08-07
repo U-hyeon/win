c46b05bb9aa5a8971b857b2f43de0345ac5706ae
U-hyeon <westwoodyousoro@gmail.com>
2022-08-07 22:42:07 +0900

    list

---
+++ algorithm/prob0.5-list/prob0.5-list/list.cpp
@@ -0,0 +1,64 @@
+#include "list.h"
+
+node_t* head = NULL, * tail = NULL;
+
+void insert_node(int n) //n = 노드가 가지는 키값
+{
+	node_t* new_node = (node_t*)malloc(sizeof(node_t)); //create new node
+
+	new_node->key = n; //designate key
+	new_node->next = NULL; //there isn't any following nodes
+	if (head == NULL) //if there is nothing on the list
+	{
+		head = new_node;
+		tail = new_node;
+	}
+	else //if there are any nodes on the list
+	{
+		tail->next = new_node;
+		tail = new_node; //insert new_node into last of the list
+	}
+}
+
+int delete_node() //delete the head of the list
+{
+	node_t* node;
+	int r;
+
+	if (head == NULL)
+	{
+		return -1;
+	} //error check. 
+
+	node = head;
+	head = head->next; //designate new head
+	if (head == NULL)
+		tail = NULL; //it there is nothing left on the list
+
+	r = node->key; //load key value to return 
+	free(node); //memory
+	return r; //return the key value of the node
+}
+
+void enqueue(int n)
+{
+	insert_node(n);
+	printf("insertion complete!\n");
+
+	node_t* node;
+	node = head;
+	do {
+		printf("[%d] ", node->key);
+		node = node->next;
+	} while (node != NULL);
+	printf("\n");
+}
+
+void dequeue()
+{
+	int tmp = delete_node();
+	if (tmp == -1)
+		printf("empty list!\n");
+	else
+		printf("return [%d]\n", tmp);
+}
---
+++ algorithm/prob0.5-list/prob0.5-list/main.cpp
@@ -0,0 +1,21 @@
+#include "list.h"
+
+void main()
+{
+	dequeue();
+	enqueue(1);
+	enqueue(2);
+	enqueue(3);
+	enqueue(4);
+	dequeue();
+	enqueue(5);
+	enqueue(6);
+	enqueue(7);
+	dequeue();
+	dequeue();
+	dequeue();
+	dequeue();
+	dequeue();
+	enqueue(8);
+}
+
