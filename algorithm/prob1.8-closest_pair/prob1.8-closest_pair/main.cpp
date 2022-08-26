06acbbea26997cf8707611df095c9f96769d3389
U-hyeon <westwoodyousoro@gmail.com>
2022-08-26 23:11:19 +0900

    220826 closest_pair_3d

--- algorithm/prob1.8-closest_pair/prob1.8-closest_pair/main.cpp
+++ algorithm/prob1.8-closest_pair/prob1.8-closest_pair/main.cpp
@@ -2,8 +2,10 @@
 //모든 쌍의 점을 비교하는 방법 제외
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
-#define SIZE 100
+#define SIZE 10000
 
 int test[10] = { 10, 20, 31, 41, 45, 60, 72, 84, 99, 110 }; // closest pair : (41, 45), distance = 4
 
@@ -13,11 +15,21 @@ struct point
 	int y;
 }; //3차원 점의 거리를 알고 싶다면 이 구조체를 이용하도록 코드를 수정할 것
 
-int closest_distance_naive(int arr[], int n)
+double distance(point a, point b)
+{
+	float r, x, y;
+	x = a.x - b.x;
+	y = a.y - b.y;
+	r = sqrt( pow(x,2) + pow(y,2) );
+	return r;
+}
+//----------------------------------------------------------------------------------------------
+
+double closest_distance_naive(int arr[], int n)
 {
 	int i, j;
-	int tmp;
-	int r = 999;
+	double tmp;
+	double r = 999999;
 	for (i=0; i<n; i++)
 	{
 		for (j = 0; j < n; j++)
@@ -32,16 +44,36 @@ int closest_distance_naive(int arr[], int n)
 	return r;
 }
 
-int closest_distance_better(int arr[], int n)
+double closest_distance_naive(point arr[], int n)
 {
 	int i, j;
-	int tmp;
-	int r = 999;
+	double tmp;
+	double r = 999999;
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < n; j++)
 		{
-			if (i - j > -r && i - j < r)
+			if (i != j)
+			{
+				tmp = distance(arr[i], arr[j]);
+				if (r > tmp) r = tmp;
+			}
+		}
+	}
+	return r;
+}
+//----------------------------------------------------------------------------------------------
+
+double closest_distance_better(int arr[], int n)
+{
+	int i, j;
+	double tmp;
+	double r = 999999;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			if (arr[i] - arr[j] > -r && arr[i] - arr[j] < r)
 			{
 				if (i != j)
 				{
@@ -54,11 +86,35 @@ int closest_distance_better(int arr[], int n)
 	return r;
 }
 
+double closest_distance_better(point arr[], int n)
+{
+	int i, j;
+	double tmp;
+	double r = 999999;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			if ( (arr[i].x - arr[j].x > -r && arr[i].x - arr[j].x < r) && (arr[i].y - arr[j].y > -r && arr[i].y - arr[j].y < r))
+			{
+				if (i != j)
+				{
+					tmp = distance(arr[i], arr[j]);
+					if (r > tmp) r = tmp;
+				}
+			}
+		}
+	}
+	return r;
+}
+//----------------------------------------------------------------------------------------------
+
 void init(int arr[], int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		arr[i] = rand() % 10000;
+		arr[i] = rand() % (SIZE * 100);
 		//중복체크
 		for (int j = 0; j < i; j++)
 		{
@@ -67,10 +123,38 @@ void init(int arr[], int n)
 	}
 }
 
+void init(point arr[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i].x = rand() % (SIZE * 100);
+		arr[i].y = rand() % (SIZE * 100);
+		//중복체크
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[i].x == arr[j].x && arr[i].y == arr[j].y) i--;
+		}
+	}
+}
+//----------------------------------------------------------------------------------------------
+
 void main()
 {
-	int points[SIZE];
+	//int points[SIZE];
+	point points[SIZE];
+	double start, end, time, tmp;
+
 	init(points, SIZE);
-	printf("naive solution : %d\n", closest_distance_naive(points, 10));
-	printf("better solution : %d\n", closest_distance_better(points, 10));
+
+	start = clock() / CLOCKS_PER_SEC;
+	tmp = closest_distance_naive(points, SIZE);
+	end = clock() / CLOCKS_PER_SEC;
+	time = end - start;
+	printf("naive solution : %2.f, time spend : %f ms\n", tmp, time);
+
+	start = clock() / CLOCKS_PER_SEC;
+	tmp = closest_distance_better(points, SIZE);
+	end = clock() / CLOCKS_PER_SEC;
+	time = end - start;
+	printf("better solution : %2.f, time spend : %f ms\n", tmp, time);
 }
